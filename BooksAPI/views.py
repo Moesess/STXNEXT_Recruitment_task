43de2c31ee87e3c8df36d32820075e7f05e345e2
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-25 22:15:45 +0200

    Update views
    Simplified List View class

--- BooksAPI/views.py
+++ BooksAPI/views.py
@@ -1,25 +1,13 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework import permissions
 
-from .models import Author, Book, Category
-from .serializers import AuthorSerializer, BookSerializer, CategorySerializer
-
-
-# Create your views here.
-class AuthorViewSet(viewsets.ModelViewSet):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-    permission_classes = [permissions.IsAuthenticated]
+from .models import Book
+from .serializers import BookSerializer
 
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookListView(viewsets.ModelViewSet):
+    """
+    List of all Books we have in database
+    """
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAuthenticated]
