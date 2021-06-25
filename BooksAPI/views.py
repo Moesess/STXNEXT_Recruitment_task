b357aa6e08a7f11a266d3ad82d7e70d82b437505
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-25 22:38:20 +0200

    Update Views
    Further simplifying views

--- BooksAPI/views.py
+++ BooksAPI/views.py
@@ -1,13 +1,19 @@
-from rest_framework import viewsets
+from django.views.generic import View
+from rest_framework import viewsets, mixins
+from rest_framework.views import APIView
 
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookListView(viewsets.ModelViewSet):
+class BookListView(mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
     """
     List of all Books we have in database
     """
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+#class LoadView(View):
