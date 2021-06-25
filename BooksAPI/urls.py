b4ff11242b84dbbe5de9ed2cdb792f13fff81572
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-25 22:13:12 +0200

    Urls update
    Changed urls to work on router from djangorestframework

--- BooksAPI/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from BooksAPI import views
-
-urlpatterns = [
-    path('books/', views.BookListView.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- STXNEXT_Recruitment_task/urls.py
+++ STXNEXT_Recruitment_task/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
+from BooksAPI.views import BookListView
+
+router = DefaultRouter()
+router.register(r'books', BookListView)
 
 urlpatterns = [
-    path('', include('BooksAPI.urls')),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
