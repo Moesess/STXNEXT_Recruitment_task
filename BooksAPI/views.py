518a5a7bdb6d2b6985e50bb0e0574fc406a852de
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-25 13:22:34 +0200

    Create Author, Book and category views
    Created viewsets for displaying Author, Book, Category models in json format

--- BooksAPI/views.py
+++ BooksAPI/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+
+from .models import Author, Book, Category
+from .serializers import AuthorSerializer, BookSerializer, CategorySerializer
+
 
 # Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
