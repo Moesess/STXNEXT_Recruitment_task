7c748d4b0cce78b65d647290c280d76ba064cdf0
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-28 17:07:02 +0200

    Changed filterset
    Authors and Categories filtering now work on AllValuesMultipleFilter.

--- BooksAPI/filtersets.py
+++ BooksAPI/filtersets.py
@@ -1,8 +1,4 @@
-import operator
-
 import django_filters
-from django.db.models import Q
-from functools import reduce
 
 from BooksAPI import models
 
@@ -12,9 +8,10 @@ class BookFilterSet(django_filters.FilterSet):
     Filtering books by,
     title, if title contains request query param
     author, if authors in request are in Book model authors.
-     Also it works with multiple authors, eg. "?author=Barton,Fisher"
+     Also it works with multiple authors, eg. "?author=Barton&author=Fisher"
     category, same as authors
     published_data, we can find book by year, month etc.
+    book_id, ITS NOT THE SAME AS MODEL DATABASE ID. Special string that every book has.
 
     sorting is enabled, available fields are "published_date". Usage: "?sort=-published_date"
     """
@@ -22,16 +19,14 @@ class BookFilterSet(django_filters.FilterSet):
         lookup_expr='contains',
     )
 
-    author = django_filters.CharFilter(
+    author = django_filters.AllValuesMultipleFilter(
         field_name='authors__name',
         lookup_expr='contains',
-        method="custom_filter"
     )
 
-    category = django_filters.CharFilter(
+    category = django_filters.AllValuesMultipleFilter(
         field_name='categories__name',
         lookup_expr='contains',
-        method="custom_filter"
     )
 
     published_date = django_filters.CharFilter(
@@ -48,20 +43,3 @@ class BookFilterSet(django_filters.FilterSet):
         model = models.Book
         fields = ('title', 'book_id', 'published_date', 'author', 'category')
 
-    def custom_filter(self, queryset, name, value):
-        """
-        Custom filter method for categories
-        :param queryset: original queryset
-        :param name: request query param key
-        :param value: request query param value
-        :return: queryset filtered by this value, using custom query. Looking for every author/category in value
-        """
-
-        if value is None:
-            return queryset
-
-        value = value.split(',')
-        name += "__contains"
-
-        queryset = models.Book.objects.filter(reduce(operator.or_, (Q(**{name: x}) for x in value)))
-        return queryset
