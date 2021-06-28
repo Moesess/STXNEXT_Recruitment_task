0ae696e93ca63b6fc3f3a416816e249bfe14842b
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-28 19:18:40 +0200

    Updated form comment

--- BooksAPI/forms.py
+++ BooksAPI/forms.py
@@ -4,6 +4,9 @@ from BooksAPI import models
 
 
 class BookForm(ModelForm):
+    """
+    Book form, used for validating data in get_data function
+    """
     class Meta:
         model = models.Book
         fields = ['title', 'average_rating', 'ratings_count', 'thumbnail']
