ae62b44e080aa666c7ed7efc6032e5f17805d0fd
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-28 17:47:24 +0200

    Basic exception for get_data

--- BooksAPI/views.py
+++ BooksAPI/views.py
@@ -24,60 +24,64 @@ class BookListView(mixins.ListModelMixin,
 
 
 def get_data(request):
-    response = requests.get(
-        f"https://www.googleapis.com/books/v1/volumes?q={request.POST['q']}").json()
+    try:
+        response = requests.get(
+            f"https://www.googleapis.com/books/v1/volumes?q={request.POST['q']}").json()
 
-    authors = []
-    books = []
-    ubooks = []
-    categories = []
+        authors = []
+        books = []
+        ubooks = []
+        categories = []
 
-    # For each book in response from api
-    for book in response["items"]:
-        book_id = str(book["id"])
-        title = str(book["volumeInfo"]["title"])
-        published_date = str(book["volumeInfo"].get("publishedDate", ""))
+        # For each book in response from api
+        for book in response["items"]:
+            book_id = str(book["id"])
+            title = str(book["volumeInfo"]["title"])
+            published_date = str(book["volumeInfo"].get("publishedDate", ""))
 
-        average_rating = float(book["volumeInfo"].get("averageRating", 0))
-        ratings_count = int(book["volumeInfo"].get("ratingsCount", 0))
-        thumbnail = str(book["volumeInfo"].get("imageLinks", {}).get("thumbnail", ""))
+            average_rating = float(book["volumeInfo"].get("averageRating", 0))
+            ratings_count = int(book["volumeInfo"].get("ratingsCount", 0))
+            thumbnail = str(book["volumeInfo"].get("imageLinks", {}).get("thumbnail", ""))
 
-        b, created = Book.objects.update_or_create(
-            book_id=book_id,
-            defaults={
-                'title': title,
-                'published_date': published_date,
-                'average_rating': average_rating,
-                'ratings_count': ratings_count,
-                'thumbnail': thumbnail
-            }
-        )
-
-        if created:
-            books.append(b)
-        else:
-            ubooks.append(b)
-
-        for author in book["volumeInfo"]["authors"]:
-            a, created = Author.objects.get_or_create(name=author)
-            a.books.add(b)
-            a.save()
+            b, created = Book.objects.update_or_create(
+                book_id=book_id,
+                defaults={
+                    'title': title,
+                    'published_date': published_date,
+                    'average_rating': average_rating,
+                    'ratings_count': ratings_count,
+                    'thumbnail': thumbnail
+                }
+            )
 
             if created:
-                authors.append(a)
+                books.append(b)
+            else:
+                ubooks.append(b)
 
-        if "categories" in book["volumeInfo"]:
-            for category in book["volumeInfo"]["categories"]:
-                c, created = Category.objects.get_or_create(name=category)
-                c.books.add(b)
-                c.save()
+            for author in book["volumeInfo"]["authors"]:
+                a, created = Author.objects.get_or_create(name=author)
+                a.books.add(b)
+                a.save()
 
                 if created:
-                    categories.append(c)
+                    authors.append(a)
 
-    res = f"Books created: {len(books)} {[str(x) for x in books]}:\n" \
-          f"Authors created: {len(authors)}: {[str(x) for x in authors]}\n" \
-          f"Categories created: {len(categories)}: {[str(x) for x in categories]}\n" \
-          f"Books updated: {len(ubooks)} {[str(x) for x in ubooks]}:\n"
+            if "categories" in book["volumeInfo"]:
+                for category in book["volumeInfo"]["categories"]:
+                    c, created = Category.objects.get_or_create(name=category)
+                    c.books.add(b)
+                    c.save()
 
-    return HttpResponse(res)
+                    if created:
+                        categories.append(c)
+
+        res = f"Books created: {len(books)} {[str(x) for x in books]}:\n" \
+              f"Authors created: {len(authors)}: {[str(x) for x in authors]}\n" \
+              f"Categories created: {len(categories)}: {[str(x) for x in categories]}\n" \
+              f"Books updated: {len(ubooks)} {[str(x) for x in ubooks]}:\n"
+
+        return HttpResponse(res)
+
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err)
