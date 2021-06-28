9b1ece234ea021297fd0aabed87d9c88945632eb
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-28 03:09:33 +0200

    Disable options

--- STXNEXT_Recruitment_task/settings.py
+++ STXNEXT_Recruitment_task/settings.py
@@ -131,5 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Rest framework settings
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_METADATA_CLASS': None,
 }
