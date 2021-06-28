6a0f0a635aa6cbd695af8839d16fa7b040e7133e
Moesess <33929420+Moesess@users.noreply.github.com>
2021-06-28 17:06:22 +0200

    Update urls

--- STXNEXT_Recruitment_task/urls.py
+++ STXNEXT_Recruitment_task/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+
 from rest_framework.routers import DefaultRouter
 
 from BooksAPI import views
@@ -10,6 +11,6 @@ router.register(r'books', views.BookListView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('get-api/', views.get_data)
+    path('db/', views.get_data, name="post_data")
 ]
 
